0a9d2855010ba1f8c82aa47f510cd3bd0b37fb2a
hephaestus <hephaestus@zoom-demo>
2023-01-07 15:06:53 -0500

    I changed the position of the shapes
    I want to get used to moving shapes around

--- cylinder.groovy
+++ cylinder.groovy
@@ -6,18 +6,18 @@ CSG myCylinder = new Cylinder(10, // Radius at the bottom
                       		40, // Height
                       		(int)30 //resolution
                       		).toCSG()//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
-                      		.movey(50)
+                      		.movey(20)
   //create a Cylinder
 CSG pyramid = new Cylinder(	20, // Radius at the bottom
                       		0, // Radius at the top
                       		40, // Height
                       		(int)4 //resolution
                       		).toCSG()//convert to CSG to display                    			 
-                      		.movex(50)
+                      		.movex(20)
    //create a Cylinder
 CSG hex = new Hexagon(	20, // Flat to flat radius
                       		40 // Height
                       		).toCSG()//convert to CSG to display                    			 
                       		.movex(50)
-                      		.movey(50)
+                      		.movey(20)
  return [simpleSyntax,myCylinder ,pyramid,hex]
